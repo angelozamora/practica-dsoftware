1ed8c5d820cb7523011bdc064eb81ab6e47f7ff2
angelozamora <angelozamora987@gmail.com>
2020-08-27 20:34:01 -0500

    free

--- src/java/Factory/IFactoryDAO.java
+++ src/java/Factory/IFactoryDAO.java
@@ -13,14 +13,14 @@ import design.IAccesoFactory;
  */
 public class IFactoryDAO {
     
-    static FactoryDB factorydb;
+    static IFactoryDAO ifactoryDAO;
     
     static{
-       factorydb = new FactoryDB(); 
+       ifactoryDAO = new IFactoryDAO(); 
     }
     
-    public static FactoryDB getInstance(){
-        return factorydb;
+    public static IFactoryDAO getInstance(){
+        return ifactoryDAO;
     }
     
     public static IAccesoFactory getAccesoFactory(String tipo){
@@ -44,3 +44,23 @@ public class IFactoryDAO {
         return MySQLFactoryDAO.getInstance();
     }
 }
+
+
+/*
+    IAccesoFactory accesoFactory = IFactoryDAO.getInstance().getAccesoFactory("MySQL");
+
+    IProductorDAO productorDAO=accesoFactory.getProductorService();
+
+    List<Productor> productorList=productorDAO.filtrarProductoresxRegionxCantidadBotellas(region, cantidad);
+
+
+    pregunta 1 = productor - filtrarProductoresxRegionxCantidadBotellas(String region,int cantidad) ----> List<Productor>
+    pregunta 2 = vino - obtenerVinoxGradoOProductor(String grado, int idProductor) ----> List<Vino>
+    pregunta 3 = productor - obtenerProductoresSinProduccion ---> List<Productor>
+    pregunta 4 =  vino - obtenerVinoMayorProduccion() ---> Vino
+    pregunta 5 = productor - productoresxNVinos(int cantidad) ----> List<Productor>
+    pregunta 6 = productor - productoresxVinosSignificativos(int cantidad) ----> List<Productor>
+    pregunta 7 = 
+    pregunta 8 = productor - obtenerProductoresSinNombreSinProduccion() ----> List<Productor>
+
+*/
